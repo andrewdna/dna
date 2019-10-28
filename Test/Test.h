a4dd59c4dfd094bde1a6470d5d82dd016a3a56a1
Andrew Ruddick <andrewruddick@andrews-mbp.home>
2019-10-28 13:34:08 +0000

    first commit

---
+++ Test/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by Andrew Ruddick on 28/10/2019.
+//  Copyright © 2019 Andrew Ruddick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test.
+FOUNDATION_EXPORT double TestVersionNumber;
+
+//! Project version string for Test.
+FOUNDATION_EXPORT const unsigned char TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test/PublicHeader.h>
+
+
